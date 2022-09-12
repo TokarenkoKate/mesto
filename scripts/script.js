bcd8260ea613c121a1dc516143cc7f66f74af7d8
Kate Tokarenko <trinatiani@yandex.ru>
2022-09-12 12:40:07 +0300

    fix: внести изменения в разметку, стили, скрипт
    -Установить правильный атрибута type для кнопок
    -Заменить теги img на фоновое изображение у кнопок
    -Добавить placeholder для input
    -Оформить карточки в виде списка
    -Заменить переменные на константы в JS
    -Измененить функциональность открытия модального окна

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const profile = document.querySelector('.profile');
+const profileEditButton = profile.querySelector('.profile__edit-btn');
+const profileName = profile.querySelector('.profile__author');
+const profileJob = profile.querySelector('.profile__job');
+const popup = document.querySelector('.popup');
+const popupCloseButton = popup.querySelector('.popup__close-button');
+const formElement = popup.querySelector('.form');
+const nameInput = popup.querySelector('.form__input_type_name');
+const jobInput = popup.querySelector('.form__input_type_job');
+
+function openPopup(evt) {
+  evt.preventDefault();
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  popup.classList.add('popup_opened');
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup();
+}
+
+profileEditButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+
+
+
+
+
+
